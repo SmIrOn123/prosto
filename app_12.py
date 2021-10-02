4fd8f065b8aaef35e5c9ba04bd3d970cb291d164
SmIrOn123 <89707090+SmIrOn123@users.noreply.github.com>
2021-10-02 20:28:51 +0500

    Update app_12.py

--- app_12.py
+++ app_12.py
@@ -1,7 +1,7 @@
 #Program for testing processor
 #
 #Russian Laung
-
+import os
 
 from time import sleep
 from random import randint
@@ -9,19 +9,28 @@ from random import randint
 print('Чем больше числа тем дольше они будут угадыватся')
 a = int(input('Число от: '))
 b = int(input('До: '))
+c = int(input('Хотите чтобы вам показывались неудачные попытоки?\n1)Да\n2)Нет\n'))
 i = 0
 
 while True:
     randomNumber = randint(a, b)
     randomUgad = randint(a, b)
 
-    if(randomUgad == randomNumber):
+    if (c == 1):
+        if(randomUgad == randomNumber):
         i += 1
         print(f'{randomUgad} = {randomNumber}\nУгадано число за {i} попыток')
         break
-    if(randomUgad < randomNumber):
+        if(randomUgad < randomNumber):
+            i += 1
+            print(f'{randomUgad} < {randomNumber}')
+        if(randomUgad > randomNumber):
+            i += 1
+            print(f'{randomUgad} > {randomNumber}')
+    if(c == 2):
+        if(randomUgad == randomNumber):
         i += 1
-        print(f'{randomUgad} < {randomNumber}')
-    if(randomUgad > randomNumber):
-        i += 1
-        print(f'{randomUgad} > {randomNumber}')
+        print(f'{randomUgad} = {randomNumber}\nУгадано число за {i} попыток')
+        break
+    else:
+        os.system('python app.py')
